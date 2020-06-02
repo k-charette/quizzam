8e1f3323bca4021a83ad51e76bae82839a76ecb1
Ken Charette <charettekenneth@gmail.com>
2020-06-01 21:04:59 -0400

    Installed tailwind and fetching data from a quiz api

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+const tailwindcss = require('tailwindcss')
+
+module.exports = {
+    theme: {},
+    variants: {},
+    plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')],
+  }
--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './styles/app.css'
 
-function App() {
+
+const App = () => {
+
+  const API_URL = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple'
+
+  const [quizInfo, setQuizInfo] = useState([])
+
+  useEffect(() => {
+    fetch(`${API_URL}`)
+    .then((response) => {
+      if (response.ok){
+        return response
+      } else {
+        let errorMessage = `${response.status} ${response.statusText}`,
+          error = new Error(errorMessage)
+          throw(error)
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      setQuizInfo(body.results)
+    })
+    .catch(error => console.log(`Error in fetch ${error.message}`))
+  }, [])
+
+  // console.log(quizInfo[0])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>This is where the question will be</div>
+      <div>
+        <div>Answer 1</div>
+        <div>Answer 2</div>
+        <div>Answer 3</div>
+        <div>Answer 4</div>
+      </div>
+      {/* {
+        quizInfo.map((trivia) => (
+          <div>
+            <div>
+              <ul>
+                <li>{trivia.incorrect_answers[0]} </li>
+                <li>{trivia.incorrect_answers[1]} </li>
+                <li>{trivia.incorrect_answers[2]} </li>
+                <li>{trivia.correct_answer}</li>
+              </ul>
+            </div>
+            <div>        
+            </div>
+          </div>       
+        ))
+      } */}
     </div>
   );
 }
